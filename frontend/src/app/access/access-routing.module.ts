03d439b6f439fd1eeef3c67dca9349aea0a9c65b
wahidahmed <wahidahmed1992@gmail.com>
2023-03-09 08:44:21 +0800

    set register com in access module

--- frontend/src/app/access/access-routing.module.ts
+++ frontend/src/app/access/access-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [];
 
 @NgModule({
+  declarations:[
+    RegisterComponent
+  ],
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
