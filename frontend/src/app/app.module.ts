1104142d50e3fb38964840aa57b08239876eb4c0
wahidahmed <wahidahmed1992@gmail.com>
2023-03-09 08:39:06 +0800

    setup login comopnent in app module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       HomeComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginComponent //because it is a stand alone component
   ],
   providers: [],
   bootstrap: [AppComponent]
