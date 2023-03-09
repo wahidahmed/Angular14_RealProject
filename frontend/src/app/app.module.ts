3c19817ee02f51621462bd24c6a99eb204aa4e45
wahidahmed <wahidahmed1992@gmail.com>
2023-03-09 09:00:52 +0800

    create contact com

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-      HomeComponent
+      HomeComponent,
+      ContactComponent
    ],
   imports: [
     BrowserModule,
